68535438cb32bcbf6483ca4f54c7357788e5278f
Skakarla2023 <skakarla2023@gmail.com>
2026-01-18 21:07:59 +0530

    Update script.js

--- 02-variables-and-scope/script.js
+++ 02-variables-and-scope/script.js
@@ -1,1 +1,38 @@
+function demo(){
+	var count = 1;
+	
+	if(true) {
+		var count = 2;
+		console.log("Inside if (var):",count);
+	}
+	
+	console.log("Outside if(var):", count);
+}
 
+demo();
+
+function demolet(){
+	let score = 10;
+	
+	if(true) {
+		let score = 20;
+		console.log("Inside if(let):", score);
+	}
+	
+	console.log("Outside if(let):",score);
+}
+
+demolet();
+
+function democonst(){
+	const appName = "MyApp";
+	const user = { name: "Satwika" };
+	
+	user.name = "JS Learner";
+	
+	console.log("App name:", appName);
+	console.log("User name:",user.name);
+	
+}
+
+democonst();
